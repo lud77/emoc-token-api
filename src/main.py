67a43c4b2964117e18f7cc791f60bd823c1bec36
Stefano Butera <stefano.butera@nethnic.com>
2023-12-30 18:23:57 +0000

    Add get-secure-token route

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 import jwt
+from cryptography.hazmat.primitives import serialization
 
 app = Flask(__name__)
+
+private_key = open('.ssh/id_rsa', 'r').read()
+key = serialization.load_ssh_private_key(private_key.encode(), password=b'')
 
 @app.route("/get-token/<secret>", methods=["POST"])
 def get_token(secret):
@@ -12,6 +16,18 @@ def get_token(secret):
         key = secret
     )
     return token
+
+@app.route("/get-secure-token/", methods=["POST"])
+def get_secure_token():
+    data = request.get_json()
+
+    token = jwt.encode(
+        payload = data,
+        key = key,
+        algorithm='RS256'
+    )
+    return token
+
 
 if __name__ == "__main__":
     app.run(debug=True)
